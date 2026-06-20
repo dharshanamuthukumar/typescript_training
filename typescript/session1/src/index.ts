a5785890de23118c36ecda18efc62f140bd1eb0d
dharshanamuthukumar <dharshanamuthukumar.2006@gmail.com>
2026-06-20 11:30:14 +0530

    typescript/session1: understand tsc --noEmit

---
+++ typescript/session1/src/index.ts
@@ -0,0 +1,4 @@
+console.log("TypeScript is running");
+const age: number = 30;
+// We use tsc --noEmit to check for TypeScript errors without generating JavaScript files.
+// It is useful during development or in CI/CD pipelines when we only want to validate the code.
